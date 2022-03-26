9baeff1da48d4fd7a6ab0553dd50d3ac764f4a38
carlos-dev <carlosandre1572@gmail.com>
2022-03-26 15:46:57 -0300

    entrada de texto com TextInput e tratamento do teclado virtual

--- src/screens/EMOMScreen.js
+++ src/screens/EMOMScreen.js
@@ -1,54 +1,72 @@
 import React from 'react';
 import {
-  View, StyleSheet,
+  View, StyleSheet, Text, TextInput, KeyboardAvoidingView,
 } from 'react-native';
-
 import Select from '../components/Select';
 import Title from '../components/Title';
+import Icon from '../utils/Icon';
 
 const EMOMScreen = () => (
-  <View style={styles.container}>
-    <Title title="EMOM" subtitle="Every minute on the minute" />
-    <Select
-      label="Alertas:"
-      current={0}
-      options={[
-        {
-          id: 0,
-          label: 'desligado',
-        },
-        {
-          id: 15,
-          label: '15s',
-        },
-        {
-          id: 30,
-          label: '30s',
-        },
-        {
-          id: 45,
-          label: '45s',
-        },
-      ]}
-      onSelect={(opt) => console.log(opt)}
-    />
-    <Select
-      label="Contagem regressiva:"
-      current={0}
-      options={[
-        { id: 1, label: 'sim' },
-        { id: 0, label: 'não' },
-      ]}
-      onSelect={(opt) => console.log(opt)}
-    />
-  </View>
+  <KeyboardAvoidingView style={{ flex: 1, flexGrow: 1 }} behavior="padding">
+    <View style={styles.container}>
+      <Title title="EMOM" subtitle="Every minute on the minute" />
+      <Icon type="fa" name="cog" size={50} color="#fff" style={[styles.icon, styles.iconCog]} />
+      <Select
+        label="Alertas:"
+        current={0}
+        options={[
+          { id: 0, label: 'desligado' },
+          { id: 15, label: '15s' },
+          { id: 30, label: '30s' },
+          { id: 45, label: '45s' },
+        ]}
+        onSelect={(opt) => console.log(opt)}
+      />
+      <Select
+        label="Contagem regressiva:"
+        current={0}
+        options={[
+          { id: 1, label: 'sim' },
+          { id: 0, label: 'não' },
+        ]}
+        onSelect={(opt) => console.log(opt)}
+      />
+      <Text style={styles.text}>Quantos minutos:</Text>
+      <TextInput style={styles.input} value="15" keyboardType="numeric" />
+      <Text style={styles.text}>minutos</Text>
+      <Icon type="fa" name="play" size={50} color="#fff" style={styles.icon} />
+      <Text>Testar</Text>
+    </View>
+  </KeyboardAvoidingView>
 );
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#d6304a',
     flex: 1,
-    paddingTop: 200,
+    paddingTop: 0,
+  },
+
+  icon: {
+    alignSelf: 'center',
+  },
+
+  iconCog: {
+    marginBottom: 17,
+  },
+
+  text: {
+    textAlign: 'center',
+    color: '#fff',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 24,
+  },
+
+  input: {
+    textAlign: 'center',
+    color: '#000',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 48,
   },
 });
 
