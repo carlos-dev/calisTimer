6be67a8200d55a1e5d598c0d505f8fbca19dc3e7
carlos-dev <carlosandre1572@gmail.com>
2022-04-03 13:28:15 -0300

    Adicionando animações ao ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { Animated } from 'react-native';
 
-const ProgressBar = ({ color, percentage, height }) => (
-  <View style={{ width: `${percentage}%` || '2%', backgroundColor: color || '#fff', height: height || 3 }} />
-);
+const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+};
+
+const ProgressBar = ({ color, percentage, height }) => {
+  const [animation] = useState(new Animated.Value(0));
+  const prevCount = usePrevious(percentage);
+
+  useEffect(() => {
+    // this one is your didupdate method for count variable
+    if (percentage !== prevCount) {
+      Animated.timing(animation, {
+        toValue: percentage,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [percentage]);
+
+  const width = animation.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0%', '100%'],
+  });
+
+  return (
+    <Animated.View style={{ width, backgroundColor: color || '#fff', height: height || 3 }} />
+  );
+};
 
 export default ProgressBar;
